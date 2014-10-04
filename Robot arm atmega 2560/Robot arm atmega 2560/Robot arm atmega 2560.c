fc6adf51644808fbf90536f434010cc46e039d35
buddhika-jay <buddhika.12@cse.mrt.ac.lk>
2014-10-05 02:09:46 +0530

    Code Professionalised :D. Checked for direction changing. Should test for PCINT

--- Robot arm atmega 2560/Robot arm atmega 2560/Robot arm atmega 2560.c
+++ Robot arm atmega 2560/Robot arm atmega 2560/Robot arm atmega 2560.c
@@ -34,126 +34,97 @@
 //ENCODER_PIN_A:	interrupt pins which are connected to encoder pin A of each motor below
 #define ENCAM0 PD0//connected to INT0
 //ENCODER_PIN_B:	define the general I/O pins which are connected to encoder pin B of each motor below
+#define ENCB_DDR DDRA
+#define ENCB_PORT PORTA
+#define ENCB_PIN PINA
 #define ENCBM0 PA0
 
-/*DEFINE THE CONTROL PINS*/
+/*CONTROL PINS*/
 //PWM (enable) pins for motors //PB 4:7 & PH 3:6
-#define RUNPORTB PORTB
-#define RUNPORTH PORTH 
-#define RUNM0 PB4
+#define ENABLE_PORT1 PORTB
+#define ENABLE_PORT2 PORTH 
+#define ENM0 PB4
 //direction controlling I/O pins PB 0:4
-#define DIRECTIONPORT PORTB
+#define DIRECTION_DDR DDRB
+#define DIRECTION_PORT PORTB
 #define DIRM0 PB0
 
+/*iNTERRUPS*/
+//External interrupts
+#define ENCAM0_EINTVECT INT0_vect
+//pin change interrupts
+#define SWM0_PCINTVECT PCINT1_vect
 
+
+/* THE LOGIC
+*The Motor structure defines the attributes
+of a Motor.
+*Seven Motor structures for seven motors.
+*Commands and Interrupts change the attribute of each Motor
+*Poll function then make the decision by looking at each Motor structure
+*/
 struct Motor{
 	int running;
-	//direction of rotation -1:left,0:none, 1:right
-	//(current rotating direction of the motor)
-	int direction;
-	//number of revolutions relative to the home
-	int relativeRevolutions;
+	int direction;//current rotating direction: LEFT or RIGHT
+	int relativeRevolutions;//number of revolutions relative to the home
 	int HomeSwitchState;
-	int maxRevolutionsRight;
-	int maxRevolutionsLeft;
+	int maxRevolutionsRight;//Right safety limit
+	int maxRevolutionsLeft;//Left safety limit
 	int ENCB;//TRUE if high
-	//command attributes these attributes should be changed when you want to execute a command
-	int directionToRotate;//set the direction the motor should rotate
+	int directionToRotate;//target direction to rotate
 };
 
 struct Motor *M0;
 
-//initializing functions
-void motorObjectSetup(struct Motor *motor);
-void pinSetup();
-void initialize();
-	//initializing utilities
-	void checkHomeSwiches(struct Motor *motor);
+/*FUNCTIONS*/
+void motorObjectSetup(struct Motor *motor);//assign initial attributes to Motors
+void pinSetup();//Setup DDR pins
+void initialize();//Checks state of all Motors before executing the program.
+void checkHomeSwiches(struct Motor *motor);//check the state of the position indicating switch of the motor.
+void pollMotor(struct Motor *motor);//THE LOGIC IS HERE. Checks all the Motors and decide what to do.
 
-
-//check functions
-void pollMotor(struct Motor *motor);
-void setENCB(struct Motor *motor);
-
-//commands
-void stopAtHome();//stops the motors if they are in home position
-void rotateRight();
+void testNow();//Testing purposes
 
 int main(void){
 	struct Motor MtestM;
 	M0 = &MtestM;
 	pinSetup();
-	sei();//enable global interrupt
-	motorObjectSetup(M0);
 	initialize();
-	//test start
-	PORTB = (1<<PB7);
-	_delay_ms(200);
-	PORTB = (0<<PB7);
-	_delay_ms(200);
-	PORTB = (1<<PB7);
-	_delay_ms(200);
-	PORTB = (0<<PB7);
-	_delay_ms(200);
-	//test end
+	motorObjectSetup(M0);
+	sei();//enable global interrupt
+	testNow();
 	while(1){
-		
-		M0->directionToRotate=LEFT;
 		pollMotor(M0);
-		_delay_ms(500);
-		M0->directionToRotate=RIGHT;
-		pollMotor(M0);
-		_delay_ms(500);
 	
 	}
 	return 0;
 	
 }
 
-
-//poll at ENCB pin of the motor
-void setENCB(struct Motor *motor){
-	if(ENCBM0 == TRUE ){
-		motor->ENCB = TRUE;
-	}
-	else{
-		motor->ENCB = FALSE ;
-	}
-}
-
-//Encoder-A Interrupt detecting function
-//this is activated when a rising edge is detected by ENCA of the motor
-//this function is written for Motor0 and you should write a dedicated function for
-//each motor
-ISR(INT0_vect){
+/*
+*Motor 0:Encoder pin A:Rising Edge Detecting Function
+*Counts encoder ticks
+*Updates the direction of the Motor
+*/
+ISR(ENCAM0_EINTVECT){
 	cli();
-	/*
-	if(M0->ENCB == TRUE){
-		M0->relativeRevolutions++;
-		//M0->direction = RISEnHIGHDIR;
-		
-	}
-	else{//put elseif
-		M0->relativeRevolutions--;
-		M0->direction = RISEnLOWDIR;
-	}*/
-	
-	//_delay_ms(5);
-	if((PINA&0x1)==0x1){
+	if((ENCB_PIN&0x1)==0x1){
 		M0->relativeRevolutions++;
 		M0->direction = RISEnHIGHDIR;
 		
 	}
-	else{//put elseif
+	else{
 		M0->relativeRevolutions--;
 		M0->direction = RISEnLOWDIR;
 	}
 	sei();
 }
-//CHECK HOME SWITCH
-//this function detects the state changes of home switch
-//when ever pin change is detected it resets the relative position to 0
-ISR(PCINT1_vect){
+
+/*
+*Motor 0:Home Switch state:Pin Change Detecting Function
+*Toggles the  homeSwitchState attribute of the motor
+*/
+ISR(SWM0_PCINTVECT){
 	cli();
 	//toggle the state of home switch state
 	if(M0->HomeSwitchState == TRUE){
@@ -165,18 +136,8 @@ ISR(PCINT1_vect){
 	sei();
 }
 
-/*
-	COMMANDS
-*/
-//this is a command function to stop at home
-void stopAtHome(struct Motor *motor){
-	if(motor->HomeSwitchState == TRUE){
-		motor->running = FALSE;
-	}
-}
 
-/*INITIALIZING*/
-	void motorObjectSetup(struct Motor *motor){
+void motorObjectSetup(struct Motor *motor){
 	motor->running = TRUE;
 	motor->directionToRotate = LEFT;
 	motor->maxRevolutionsLeft = -1000;
@@ -198,10 +159,10 @@ void pinSetup(){
 	
 	
 	//Setup external interrupts
-	EIMSK = 0xFF;//Enabling interrups 7:0
+	EIMSK = 0xFF;//Enabling interrupts 7:0
 	EICRA = 0xFF;//Enabling Rising edge interrupts for INT 3:0
-	EICRB = 0xFF;//Enabling Rising edge interrupst for INT 7:4
-	//setup pin change interrups
+	EICRB = 0xFF;//Enabling Rising edge interrupts for INT 7:4
+	//setup pin change interrupts
 	PCMSK1 = 0xFF;//Enabling PCinterrupts 15:8, but we want only 15:9/J6:J0
 	PCICR = (1<<PCIE1);//Enabling PCinterrupts 15:8, but we want only 15:9/J6:J0 
 }
@@ -209,12 +170,8 @@ void initialize(){
 	checkHomeSwiches(M0);
 	
 }
-
-/*INIT UTILITIES*/
-
+//more work here:How to pass SWM0????
 void checkHomeSwiches(struct Motor *motor){
-	//set the switch state before begin
-	//have to write following code for each motor
 	if(SWM0 == SWITCHE_PRESSED){
 		motor->HomeSwitchState = SWITCHE_PRESSED;
 	}
@@ -223,39 +180,36 @@ void checkHomeSwiches(struct Motor *motor){
 	}
 }
 
-//main poll function
-//only for testing M0
 void pollMotor(struct Motor *motor){
-	//run stop motor
+	//run/stop motor
 	if(motor->running == TRUE){
-		RUNPORTB = (TRUE<<RUNM0);
+		ENABLE_PORT1 = (TRUE<<ENM0);
 	}
 	else{
-		RUNPORTB = (FALSE<<RUNM0);
+		ENABLE_PORT1 = (FALSE<<ENM0);
 	}
 	//change the rotating direction
 	if(motor->directionToRotate == RIGHT){//rotate right
-		DIRECTIONPORT = (RIGHT<<DIRM0)|(DIRECTIONPORT);
-		//PORTB = (1<<PB0)|(PORTB);
+		DIRECTION_PORT = (RIGHT<<DIRM0)|(DIRECTION_PORT);
 	}
 	else if(motor->directionToRotate == LEFT){//rotate left
-		DIRECTIONPORT = (LEFT<<DIRM0)|(DIRECTIONPORT);
-		//PORTB = (0<<PB0)|(PORTB);
+		DIRECTION_PORT = (LEFT<<DIRM0)|(DIRECTION_PORT);
 	}
-	//stop the motor if limits reached
+	//stop the motor if safety limits reached
 	if(((motor->relativeRevolutions)>= (motor->maxRevolutionsRight))||((motor->relativeRevolutions)<=(motor->maxRevolutionsLeft))){
 		motor->running = FALSE;
 	}
-	//test check the switch
-	if(motor->HomeSwitchState == SWITCHE_PRESSED){//test
-		PORTB = ((1<<PB7)|(PORTB));
-		//_delay_ms(500);
-	}
-	else{
-		PORTB = ((0<<PB7)|(PORTB));
-		//_delay_ms(500);
-	}
-	//switch test ends here
+
 	
+}
+
+void testNow(){
+	PORTB = ((1<<PB7)|(PORTB));
+	_delay_ms(500);
+	PORTB = ((0<<PB7)|(PORTB));
+	_delay_ms(500);
+	
+	M0->directionToRotate = LEFT;
+	pollMotor(M0);	
 }
 
