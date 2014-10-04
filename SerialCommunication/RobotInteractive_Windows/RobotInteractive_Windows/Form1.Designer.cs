3caf3c872578de6b409d9c16781efc4b223c36cc
KRVPerera <rukshan.viduranga@gmail.com>
2014-10-04 19:15:28 +0530

    Succesfully detecting COM ports in Visual Studio

---
+++ SerialCommunication/RobotInteractive_Windows/RobotInteractive_Windows/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace RobotInteractive_Windows
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GetPortsB = new System.Windows.Forms.Button();
+            this.PortNamesRTB = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // GetPortsB
+            // 
+            this.GetPortsB.Location = new System.Drawing.Point(171, 12);
+            this.GetPortsB.Name = "GetPortsB";
+            this.GetPortsB.Size = new System.Drawing.Size(75, 23);
+            this.GetPortsB.TabIndex = 0;
+            this.GetPortsB.Text = "Get Ports";
+            this.GetPortsB.UseVisualStyleBackColor = true;
+            this.GetPortsB.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // PortNamesRTB
+            // 
+            this.PortNamesRTB.Location = new System.Drawing.Point(33, 61);
+            this.PortNamesRTB.Name = "PortNamesRTB";
+            this.PortNamesRTB.Size = new System.Drawing.Size(213, 172);
+            this.PortNamesRTB.TabIndex = 2;
+            this.PortNamesRTB.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.PortNamesRTB);
+            this.Controls.Add(this.GetPortsB);
+            this.Name = "Form1";
+            this.Text = "b";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button GetPortsB;
+        private System.Windows.Forms.RichTextBox PortNamesRTB;
+    }
+}
+
---
+++ SerialCommunication/RobotInteractive_Windows/RobotInteractive_Windows/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+
+namespace RobotInteractive_Windows
+{
+    
+    public partial class Form1 : Form
+    {
+        SerialPort ComPort = new SerialPort(); 
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] PortNames = null;
+            int index = -1;
+            string PortNm = null;
+
+            PortNames = SerialPort.GetPortNames();
+            try
+                {
+            do
+            {
+                index += 1;
+                
+                    PortNamesRTB.Text += PortNames[index] + "\n";
+              
+            } while (!((PortNames[index] == PortNm)) || (index == PortNames.GetUpperBound(0)));
+                }
+            catch (IndexOutOfRangeException)
+            {
+                MessageBox.Show("Array");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
