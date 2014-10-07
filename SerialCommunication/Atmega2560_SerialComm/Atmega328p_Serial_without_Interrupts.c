993caf040cc1eb8c58b0b4f9645c6de4666d648b
KRVPerera <rukshan.viduranga@gmail.com>
2014-10-07 21:19:29 +0530

    removed extra interrupt functions

--- SerialCommunication/Atmega2560_SerialComm/Atmega328p_Serial_without_Interrupts.c
+++ SerialCommunication/Atmega2560_SerialComm/Atmega328p_Serial_without_Interrupts.c
@@ -15,26 +15,24 @@
 #define BAUD 9600							//BAUD rate
 #define BAUDRS (FOSC/16/BAUD -1)			//BAUD rate scaler
 
-volatile char ReceivedChar = 'c' ;
+volatile char ReceivedChar;
 
 void LED_init();
 void USART_init();
-void USART_Interrupt_EN();
 void run_without_interrupt();
-void run_with_interrupt();
+
 
 
 int main(void)
 {
 	LED_init();
 	USART_init();	
-	//USART_Interrupt_EN();	
 	
     while(1)
     {
 		
 		run_without_interrupt();
-		//run_with_interrupt();
+
 		
 		PORTB = 0x00;
     }
@@ -47,22 +45,6 @@ void USART_init(){
 	UCSR0C = ((1<<UCSZ00)|(1<<UCSZ01));		  // set frame data bit:8 and stop bit 1
 	UBRR0H = (uint8_t)(BAUDRS>>8);	  //set baud rate HIGH bits
 	UBRR0L = (uint8_t)(BAUDRS);	  //set baud rate low bits
-	
-	//UCSR0C |= (1<<UMSEL00);
-}
-
-void USART_Interrupt_EN(){
-	//cli();
-	UCSR0B |= (1 << RXCIE0);// |(1 << TXCIE0)); // RX complete interrupt
-	//UCSR0B |= (1 << RXCIE0);// |(1 << TXCIE0)); // RX complete interrupt
-	//sei();
-}
-
-void run_with_interrupt(){
-	if( (ReceivedChar == 'c') || (ReceivedChar == 'C') ){
-		PORTB |= (1<<PB7);
-		_delay_ms(500);
-	}	
 }
 
 
@@ -83,21 +65,3 @@ void LED_init(){
 	PORTB = 0x00;
 }
 
-ISR(USART0_RX_vect,ISR_BLOCK){
-	ReceivedChar = UDR0;
-	UDR0 = ReceivedChar;
-	ReceivedChar = 'c';	
-	PORTB = 0xFF;
-	_delay_ms(500);
-}
-
-
-ISR(_VECTOR(27)){
-	///cli();
-	ReceivedChar = UDR0;
-	UDR0 = ReceivedChar;
-	ReceivedChar = 'c';
-	PORTB = 0xFF;
-	_delay_ms(500);
-	//sei();
-}
