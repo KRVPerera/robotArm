92623512fa262817d4fc55c707a6ed31abc82c70
KRVPerera <rukshan.viduranga@gmail.com>
2014-10-07 21:17:02 +0530

    serial working on polling method

--- SerialCommunication/Atmega2560_SerialComm/Atmega328p_Serial_without_Interrupts.c
+++ SerialCommunication/Atmega2560_SerialComm/Atmega328p_Serial_without_Interrupts.c
@@ -1,28 +1,103 @@
-#include<avr/io.h>
+/*
+ * Atmega2560SerialCommunication.c
+ *
+ * Created: 10/7/2014 12:37:30 PM
+ *  Author: krv
+ */ 
 #define F_CPU 16000000UL
-#define USART_BAUDRATE 9600
-#define BAUD_PRESCALE (((F_CPU/(USART_BAUDRATE*16UL)))-1)
+#include <avr/io.h>
+#include <stdint.h>                     // needed for uint8_t
+#include <util/delay.h>
+#include <avr/interrupt.h>
 
-int main(void){
-	char recieved_byte;
+
+#define FOSC 16000000                       // Clock Speed
+#define BAUD 9600							//BAUD rate
+#define BAUDRS (FOSC/16/BAUD -1)			//BAUD rate scaler
+
+volatile char ReceivedChar = 'c' ;
+
+void LED_init();
+void USART_init();
+void USART_Interrupt_EN();
+void run_without_interrupt();
+void run_with_interrupt();
+
+
+int main(void)
+{
+	LED_init();
+	USART_init();	
+	//USART_Interrupt_EN();	
 	
-	UCSR0B |= (1<<RXEN0)  | (1<<TXEN0);
-	UCSR0C |= (1<<UCSZ00) | (1<<UCSZ01);
-	UBRR0H  = ( BAUD_PRESCALE >> 8);
-	UBRR0L  = BAUD_PRESCALE;
+    while(1)
+    {
+		
+		run_without_interrupt();
+		//run_with_interrupt();
+		
+		PORTB = 0x00;
+    }
 	
-	for(;;){
-		// wait until a byte is ready to read
-		while( ( UCSR0A & ( 1 << RXC0 ) ) == 0 ){}
-		
-		// grab the byte from the serial port
-		recieved_byte = UDR0;
-		
-		// wait until the port is ready to be written to
-		while( ( UCSR0A & ( 1 << UDRE0 ) ) == 0 ){}
-		
-		// write the byte to the serial port
-		UDR0 = recieved_byte;
+}
+
+void USART_init(){
+	
+	UCSR0B = ((1<<RXEN0)|(1<<TXEN0));//|(1 << RXCIE0) |(1 << TXCIE0));			  //enable USART receiver and transmitter
+	UCSR0C = ((1<<UCSZ00)|(1<<UCSZ01));		  // set frame data bit:8 and stop bit 1
+	UBRR0H = (uint8_t)(BAUDRS>>8);	  //set baud rate HIGH bits
+	UBRR0L = (uint8_t)(BAUDRS);	  //set baud rate low bits
+	
+	//UCSR0C |= (1<<UMSEL00);
+}
+
+void USART_Interrupt_EN(){
+	//cli();
+	UCSR0B |= (1 << RXCIE0);// |(1 << TXCIE0)); // RX complete interrupt
+	//UCSR0B |= (1 << RXCIE0);// |(1 << TXCIE0)); // RX complete interrupt
+	//sei();
+}
+
+void run_with_interrupt(){
+	if( (ReceivedChar == 'c') || (ReceivedChar == 'C') ){
+		PORTB |= (1<<PB7);
+		_delay_ms(500);
+	}	
+}
+
+
+void run_without_interrupt(){
+	while( ( UCSR0A & ( 1 << RXC0 ) )==0){}
+	ReceivedChar = UDR0;
+	UDR0 = ReceivedChar;
+
+	if( ReceivedChar == 'c' || ReceivedChar == 'C' ){
+		PORTB |= (1<<PB7);
+		_delay_ms(500);
 	}
-	return 0;   /* never reached */
+}
+
+
+void LED_init(){
+	DDRB = 0xFF;
+	PORTB = 0x00;
+}
+
+ISR(USART0_RX_vect,ISR_BLOCK){
+	ReceivedChar = UDR0;
+	UDR0 = ReceivedChar;
+	ReceivedChar = 'c';	
+	PORTB = 0xFF;
+	_delay_ms(500);
+}
+
+
+ISR(_VECTOR(27)){
+	///cli();
+	ReceivedChar = UDR0;
+	UDR0 = ReceivedChar;
+	ReceivedChar = 'c';
+	PORTB = 0xFF;
+	_delay_ms(500);
+	//sei();
 }
